33668caae9b8dd7d7c7a439c997b3d1c8f27e5fc
09ahmad <122668682+09ahmad@users.noreply.github.com>
2023-10-21 17:43:03 +0000

    basics of javascript

---
+++ 01_basics/var_datatype_01.js
@@ -0,0 +1,20 @@
+// let sum,num,x;
+// x=20
+// y=30
+// num=x/2
+// sum=x+y;
+// console.log(num + sum );
+// let place=["azamgarh","noida","rampur"];
+// console.log(place);
+// let student={
+//    firstname:"shaikh",
+//    lastname:"ahmad",
+//    age:21
+//     , mark:"spot on the right hand"
+// }
+// console.log(student.lastname)
+// function func(){
+//     let num=321;
+//     console.log(num);
+// }
+// func();
