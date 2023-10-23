e9d0b8017e3f5831a483c9fa4a1853b15c966c19
09ahmad <122668682+09ahmad@users.noreply.github.com>
2023-10-23 11:39:13 +0000

    coversion data type

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,7 @@
+// let score="ahmad"
+// // console.log(typeof score)
+// let valueInNumber=Number(score)
+// // console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+let isLoggedIn="ahmad"
+let 
