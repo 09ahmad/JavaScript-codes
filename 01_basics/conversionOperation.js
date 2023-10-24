6e01d5c31aa6518ccd85b7b142941d4f84d85edb
ahmad <122668682+09ahmad@users.noreply.github.com>
2023-10-24 16:40:29 +0000

    conversion02

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -3,5 +3,31 @@
 // let valueInNumber=Number(score)
 // // console.log(typeof valueInNumber);
 // console.log(valueInNumber)
-let isLoggedIn="ahmad"
-let 
+// let isLoggedIn=""
+// let booleanIsloggedIn=Boolean(isLoggedIn);
+// console.log( booleanIsloggedIn)
+// let someNumber=33
+// let stringNumber=String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// let value=2
+// let negativeValue=-value
+// console.log(negativeValue)
+// console.log(2+2)
+// console.log(2*2)
+// console.log(2-2)
+// console.log(2**2)
+// console.log(2/3)
+// console.log(2%3)
+// let str1="shaikh"
+// let str2="ahmad"
+// str3=str1+str2
+// console.log(str3)
+// console.log(1+"2")
+// console.log("1"+2)
+// console.log(1+2+"2")
+// console.log( (3+4) * 5 %3)
+// console.log(+true);
+// console.log(+"");
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
